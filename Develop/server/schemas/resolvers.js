2735c81261bdc87d3c87199ead87e05793d3137a
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-05-07 09:39:17 -0500

    commit

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,4 +1,15 @@
-const { Book, User } = require ('../models')
+const { AuthenticatioError } = require('apollo-server-express');
+const { Book, User } = require('../models')
+const { signToken } = require('../utils/auth')
 
+const resolvers = {
+    Query: {
+User: async (parent,{username}) => {
+    return User.findOne({username})
+}
+    },
+    Mutation: {
 
+    },
+}
 module.exports = resolvers;
