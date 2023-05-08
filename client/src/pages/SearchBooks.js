3eb5da347ef01eab65b438e01f009484b8d8f3a6
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-05-07 22:18:56 -0500

    commit

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -7,7 +7,7 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -62,8 +62,8 @@ const SearchBooks = () => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    // const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const [bookToSave, { error}] = useMutation(SAVE_BOOK);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
